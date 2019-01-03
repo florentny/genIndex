4d45f7f48f1e7cc1dd696eaa443376b6afdfe966
Florent Charpin <dev@florent.us>
2019-01-02 21:58:20 -0500

    viewport

--- genIndex.py
+++ genIndex.py
@@ -15,18 +15,9 @@ def get_start(header, dir_path):
 <html>
 <head>
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=640">
 <title>Photo Album</title>
-<style type="text/css">
-    body {
-         padding: 2rem;
-    }
 
-    .pig-wrapper {
-        position: relative;
-        margin-top: 20px;
-    }
-</style>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script
   src="https://code.jquery.com/jquery-3.3.1.min.js"
